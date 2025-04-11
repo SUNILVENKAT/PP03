cdc3271a00e065ec35b5a3d48a24f0897a8201b0
Sunil Venkatesh <sunil.v.m20@gmail.com>
2025-04-10 22:38:32 -0600

    Changes for classes other than GUI class

--- src/PP03/PayRecord.java
+++ src/PP03/PayRecord.java
@@ -14,7 +14,7 @@ public class PayRecord {
     private double montlyIncome;
     private int numMonths;
     
-       
+    private static int payRecordCount = 0;
     
     public static final int REG_HOURS = 40;
     public static final double OT_RATE = 1.25;
@@ -30,7 +30,7 @@ public class PayRecord {
     	this.montlyIncome = 0;
     	this.numMonths = 0;
     	this.payTax = new TaxIncome();
-  
+    	this.payRecordCount++;
     }
     
     // pay record constructor for full time employee
@@ -44,7 +44,7 @@ public class PayRecord {
  	this.montlyIncome = mIncome;
  	this.numMonths = mNum;
  	this.payTax = new TaxIncome();
-
+ 	this.payRecordCount++;
  }
 
 
@@ -52,16 +52,100 @@ public class PayRecord {
   // 2- add override method toString()
   // 3- complete the code in the following methods: grossPay() and netPay()
     
-    // complete the code to compute the gross pay for the employee based on the employee status
-	public double grossPay(){
-		return 0;
+  public int getrID() {
+		return rID;
 	}
-    
-  // complete the code in this method to compute the net pay of the employee after taxes (state and federal)
+
+	public void setrID(int rID) {
+		this.rID = rID;
+	}
+
+	public Employee getEmployee() {
+		return employee;
+	}
+
+	public void setEmployee(Employee employee) {
+		this.employee = employee;
+	}
+
+	public PayPeriod getPayPeriod() {
+		return payPeriod;
+	}
+
+	public void setPayPeriod(PayPeriod payPeriod) {
+		this.payPeriod = payPeriod;
+	}
+
+	public TaxIncome getPayTax() {
+		return payTax;
+	}
+
+	public void setPayTax(TaxIncome payTax) {
+		this.payTax = payTax;
+	}
+
+	public double getPayHours() {
+		return payHours;
+	}
+
+	public void setPayHours(double payHours) {
+		this.payHours = payHours;
+	}
+
+	public double getPayRate() {
+		return payRate;
+	}
+
+	public void setPayRate(double payRate) {
+		this.payRate = payRate;
+	}
+
+	public double getMontlyIncome() {
+		return montlyIncome;
+	}
+
+	public void setMontlyIncome(double montlyIncome) {
+		this.montlyIncome = montlyIncome;
+	}
+
+	public int getNumMonths() {
+		return numMonths;
+	}
+
+	public void setNumMonths(int numMonths) {
+		this.numMonths = numMonths;
+	}
+
+	public static int getPayRecordCount() {
+		return payRecordCount;
+	}
+
+	@Override
+	public String toString() {
+		return "PayRecord [rID=" + rID + ", employee=" + employee + ", payPeriod=" + payPeriod + ", payTax=" + payTax
+				+ ", payHours=" + payHours + ", payRate=" + payRate + ", montlyIncome=" + montlyIncome + ", numMonths="
+				+ numMonths + "]";
+	}
+
+	// complete the code in this method to compute the net pay of the employee after taxes (state and federal)
      public double netPay(){
-		  return 0;
+		  return grossPay() - payTax.compIncomeTax(grossPay());
   }
-  
+     
+     // complete the code to compute the gross pay for the employee based on the employee status
+ 	public double grossPay(){
+ 		double grossPay = 0;
+ 		if(employee.getEmpStatus() == Status.FullTime) {
+ 			grossPay = numMonths * montlyIncome;
+ 		}else if(employee.getEmpStatus() == Status.Hourly) {
+ 			if(payHours > REG_HOURS) {
+ 				grossPay = (payRate * REG_HOURS) + (payRate * OT_RATE * (payHours - REG_HOURS));
+ 			}else {
+ 				grossPay = payRate * payHours;
+ 			}
+ 		}
+ 		return grossPay;
+ 	}
  
 
 }
